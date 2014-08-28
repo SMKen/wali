101999cc582fcd26fc944f463c50ec26bc4183be
SMKen <zhengchaoken@163.com>
2014-08-28 22:34:00 +0800

    entity export to db

---
+++ smdiary/src/main/java/com/wali/smdiary/entity/SmDiary.java
@@ -0,0 +1,177 @@
+package com.wali.smdiary.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 日志表.
+ * 
+ * @author Ken
+ * @since 2014年8月28日
+ */
+@Entity(name = "wali_diary")
+public class SmDiary implements Serializable
+{
+
+	private static final long serialVersionUID = -1550154992225104488L;
+	@Id
+	@GeneratedValue(generator = "paymentableGenerator")
+	@GenericGenerator(name = "paymentableGenerator", strategy = "assigned")
+	@Column(name = "uid", length = 32)
+	private String uid;
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date diaryDay;
+	@Column(length = 32)
+	private String weather;
+	@Column(length = 64)
+	private String mood;
+	@Column(length = 32)
+	private String admin;
+	@Column(length = 64)
+	private String outline;
+	@Column(length = 64)
+	private String categorys;
+	@Column
+	private String diary;
+	@Column
+	private int viewtimes;
+	@Column
+	private int diarycount;
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createTime;
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updateTime;
+
+	public String getUid()
+	{
+		return uid;
+	}
+
+	public void setUid(String uid)
+	{
+		this.uid = uid;
+	}
+
+	public Date getDiaryDay()
+	{
+		return diaryDay;
+	}
+
+	public void setDiaryDay(Date diaryDay)
+	{
+		this.diaryDay = diaryDay;
+	}
+
+	public String getWeather()
+	{
+		return weather;
+	}
+
+	public void setWeather(String weather)
+	{
+		this.weather = weather;
+	}
+
+	public String getMood()
+	{
+		return mood;
+	}
+
+	public void setMood(String mood)
+	{
+		this.mood = mood;
+	}
+
+	public String getAdmin()
+	{
+		return admin;
+	}
+
+	public void setAdmin(String admin)
+	{
+		this.admin = admin;
+	}
+
+	public String getOutline()
+	{
+		return outline;
+	}
+
+	public void setOutline(String outline)
+	{
+		this.outline = outline;
+	}
+
+	public String getCategorys()
+	{
+		return categorys;
+	}
+
+	public void setCategorys(String categorys)
+	{
+		this.categorys = categorys;
+	}
+
+	public String getDiary()
+	{
+		return diary;
+	}
+
+	public void setDiary(String diary)
+	{
+		this.diary = diary;
+	}
+
+	public int getViewtimes()
+	{
+		return viewtimes;
+	}
+
+	public void setViewtimes(int viewtimes)
+	{
+		this.viewtimes = viewtimes;
+	}
+
+	public int getDiarycount()
+	{
+		return diarycount;
+	}
+
+	public void setDiarycount(int diarycount)
+	{
+		this.diarycount = diarycount;
+	}
+
+	public Date getCreateTime()
+	{
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime)
+	{
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime()
+	{
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime)
+	{
+		this.updateTime = updateTime;
+	}
+
+}
---
+++ smdiary/src/main/java/com/wali/tools/hibernate/ExportDb.java
@@ -0,0 +1,22 @@
+package com.wali.tools.hibernate;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+/**
+ * 导出实体的数据库结构.
+ * 
+ * @author Ken
+ * @since 2014年8月28日
+ */
+public class ExportDb
+{
+	public static void main(String[] args)
+	{
+		Configuration cfg = new Configuration().configure("export.xml");
+		SchemaExport export = new SchemaExport(cfg);
+		// true,print ; true export to db
+		export.create(true, false);
+	}
+
+}
