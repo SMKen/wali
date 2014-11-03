96d8910f7d8d2e10259c0df3075c164b501eb4d2
SMKen <zhengchaoken@163.com>
2014-11-03 17:58:43 +0800

    Base version servlet version.

---
+++ Wali/smblog/src/main/java/k/lang/DateUtil.java
@@ -0,0 +1,76 @@
+package k.lang;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * 时间工具类.
+ * 
+ * @author Ken
+ * @since 2014年9月13日
+ */
+public class DateUtil
+{
+
+	/** yyyy-MM-dd */
+	public final static String FORMAT_DATE = "yyyy-MM-dd";
+	/** yyyy-MM */
+	public final static String FORMAT_DATE_YM = "yyyy-MM";
+	/** yyyy-MM-dd HH:mm:ss */
+	public final static String FORMAT_DATETIME = "yyyy-MM-dd HH:mm:ss";
+	/** yyyy年MM月dd日 */
+	public final static String FORMAT_DATE_ZH = "yyyy年MM月dd日";
+	/** yyyy年MM月 */
+	public final static String FORMAT_DATE_ZHMONTH = "yyyy年MM月";
+	/** yyyy年MM月dd日 HH时mm分ss秒 */
+	public final static String FORMAT_DATETIME_ZH = "yyyy年MM月dd日 HH时mm分ss秒";
+
+	/**
+	 * @see {@link #FORMAT_DATE_ZH}
+	 */
+	public static String formatDateZH(Date date)
+	{
+		java.text.SimpleDateFormat sfdate = new java.text.SimpleDateFormat(FORMAT_DATE_ZH);
+		return sfdate.format(date);
+	}
+	
+	/**
+	 * @see {@link #FORMAT_DATE_ZHMONTH}
+	 */
+	public static String formatDateZHMonth(Date date)
+	{
+		java.text.SimpleDateFormat sfdate = new java.text.SimpleDateFormat(FORMAT_DATE_ZHMONTH);
+		return sfdate.format(date);
+	}
+
+	/**
+	 * @see {@link #FORMAT_DATE_ZH}
+	 */
+	public static Date formatDateStringZH(String date)
+	{
+		java.text.SimpleDateFormat sfdate = new java.text.SimpleDateFormat(FORMAT_DATE);
+		try
+		{
+			return sfdate.parse(date);
+		} catch (ParseException e)
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * @see {@link #FORMAT_DATE_YM}
+	 */
+	public static Date formatDateStringYearMonth(String date)
+	{
+		java.text.SimpleDateFormat sfdate = new java.text.SimpleDateFormat(FORMAT_DATE_YM);
+		try
+		{
+			return sfdate.parse(date);
+		} catch (ParseException e)
+		{
+			return null;
+		}
+	}
+	
+}
